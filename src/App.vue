bfc9e8ded00c7e304364b437cd06b2b3f0554a9d
KristianSic <sic.kristian@gmail.com>
2023-01-15 12:46:08 +0100

    base

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+div
+HelloWorld()
+</template>
+
+<style lang="less" scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
+
+<script lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+
+<template lang="pug">
+
+h1 {{ msg }}
+.card
+  button(type='button' @click='count++') count is {{ count }}
+
+</template>
+
+<style lang="less" scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
+
+
+
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+
+</script>
